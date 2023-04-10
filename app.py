b0106a84f84090d9dd8b88dae34a2d7f4659289e
cs21d201 <cs21d201@smail.iitm.ac.in>
2023-04-10 10:53:59 +0000

    expressive-1

--- app.py
+++ app.py
@@ -1,199 +1,120 @@
 import os
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout='wide')
+st.markdown('## AI4Bharat - Expressive TTS Samples')
 
-st.markdown("## AI4Bharat - IndicTTS")
+samples_dir = 'expressive_ta_dravidian_subjective'
+files = os.listdir(samples_dir)
+emotions = ['happy', 'sad', 'angry', 'fear', 'disgust', 'surprise']
+speakers = ['emotive_female', 'female', 'male']
 
-cols = st.columns(6)
-cols[0].markdown('**Language**')
-cols[1].markdown('**Speaker**')
-cols[2].markdown('**AI4Bharat**')
-cols[3].markdown('**GT**')
-cols[4].markdown('**Vakyansh**')
-cols[5].markdown('**Donlab**')
+def get_index(f):
+    return int(f.split('_')[0])
 
-languages = os.listdir('demo')
-
-language2title = {
-    'as': 'Assamese',
-    'bn': 'Bengali',
-    'brx': 'Bodo',
-    'gu': 'Gujarati',
-    'hi': 'Hindi',
-    'kn': 'Kannada',
-    'ml': 'Malayalam',
-    'mni': 'Manipuri',
-    'mr': 'Marathi',
-    'or': 'Odia',
-    'raj': 'Rajasthani',
-    'ta': 'Tamil',
-    'te': 'Telugu'
-}
-
-for language in sorted(languages):
-    language_dir = os.path.join('demo', language)
-    files = os.listdir(language_dir)
-    # cols2 = st.columns(9)
-    
-    cols = st.columns(6)
-    file = None
+def filter_speakers(files, speaker):
+    filtered = []
     for file in files:
-        if 'female' in file and file.endswith('_a.wav'):
-            break
-        else:
-            file = None
-    if file is not None:
-        cols[0].markdown(language2title[language])
-        cols[1].markdown('Female')
-        filepath = os.path.join(language_dir, file)
-        for idx, append in enumerate('abcd'):
-            sample_path = filepath.replace('_a.wav', f'_{append}.wav')
-            if os.path.exists(sample_path):
-                cols[idx+2].audio(sample_path)
-            else:
-                cols[idx+2].markdown('-')
+        if speaker == 'male':
+            if ('male' in file and 'female' not in file) or 'emotive_female' in file:
+                filtered.append(file)
+        elif 'female' in file:
+            filtered.append(file)
+    return filtered
 
-    cols = st.columns(6)
-    file = None
-    for file in files:
-        if 'male' in file and not 'female' in file and file.endswith('_a.wav'):
-            break
-        else: 
-            file = None
-    if file is not None:
-        cols[0].markdown(language2title[language])
-        cols[1].markdown('Male')
-        filepath = os.path.join(language_dir, file)
-        for idx, append in enumerate('abcd'):
-            sample_path = filepath.replace('_a.wav', f'_{append}.wav')
-            if os.path.exists(sample_path):
-                cols[idx+2].audio(sample_path)
-            else:
-                cols[idx+2].markdown('-')
+option = st.selectbox('Language: ', ['Kannada', 'Malayalam', 'Tamil', 'Telugu'])
+if option == 'Tamil':
+    st.markdown('### Tamil')
+    cols = st.columns(8)
+    cols[0].markdown('##### Index')
+    cols[1].markdown('##### Speaker')
+    cols[2].markdown('##### Emotion')
+    cols[3].markdown('##### v2: Expressive Voice (Emotive)')
+    cols[4].markdown('##### v2: Expressive Voice (Neutral)')
+    cols[5].markdown('##### v2: IndicTTS Voice (Emotive)')
+    cols[6].markdown('##### v2: IndicTTS Voice (Neutral)')
+    cols[7].markdown('##### v1: IndicTTS Voice')
+    st.markdown("""---""")
+    ta_files = sorted([f for f in files if 'ta' in f])
+    indices = sorted(list(set([get_index(f) for f in ta_files])))
+    for index in indices:
+        index_files = [f for f in ta_files if index == get_index(f)]
+        for speaker in ['female', 'male']:
+            speaker_files = filter_speakers(index_files, speaker)
+            emotion = speaker_files[0].split('_')[-1].replace('.wav', '')
+            cols = st.columns(8)
+            cols[0].markdown(f'{index}')
+            cols[1].markdown(f'**{speaker.capitalize()}**')
+            cols[2].markdown(emotion)
+            print(speaker_files)
+        
+            speaker_file = f'{str(index)}_ta_emotive_female_{emotion}.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[3].audio(f.read())
 
-    
+            speaker_file = f'{str(index)}_ta_emotive_female_neutral.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[4].audio(f.read())
 
+            speaker_file = f'{str(index)}_ta_{speaker}_{emotion}.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[5].audio(f.read())
+            
+            speaker_file = f'{str(index)}_ta_{speaker}_neutral.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[6].audio(f.read())
+            
+            speaker_file = f'{str(index)}_ta_{speaker}_v1.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[7].audio(f.read())
 
-# option = st.selectbox(
-#      'Select a language:',
-#      ('Tamil', 'Hindi', 'Marathi')
-#      )
-
-# if option == 'Tamil':
-#     ### START: Tamil
-#     st.markdown("### Tamil")
-
-#     models = [
-#         'AI4Bharat|Male|FastPitch+HiFiGAN|samples/samples_indictts_ta_male_fastpitch', 
-#         'AI4Bharat|Multi|FastPitch+HiFiGAN|samples/samples_googletts_ta_multi_fastpitch', 
-#         'Vakyansh|Male|GlowTTS+HiFiGAN|samples/samples_vakyansh_ta_male_glowtts', 
-#         'DonLab|Male|Tacotron2+WaveGlow|samples/samples_donlab_ta_male_tacotron2'
-#     ]
-
-#     texts = [
-#         '1) செஸ் ஒலிம்பியாட் போட்டியை சென்னை முதல் முறையாக நடத்திய பிறகு, ஜவாஹர்லால் நேரு உள்விளையாட்டு அரங்கில் பிரமாண்டமான முறையில் அதன் நிறைவு விழா நடைபெற்றது.',
-#         '2) இலங்கையின் முன்னாள் ஜனாதிபதி கோட்டாபய ராஜபக்ஷ, தாய்லாந்திற்கு செல்லவுள்ளதாக ராய்ட்டர்ஸ் செய்தி வெளியிட்டுள்ளது.',
-#         '3) காமன்வெல்த் போட்டிகளில் இந்தியாவுக்கு மூன்றாவது தங்கப் பதக்கத்தை பெற்று தந்துள்ளார் பளுதூக்கும் வீரர் அச்சிண்டா செயுலி.',
-#         '4) தமிழ்நாட்டின் ஆதிச்சநல்லூர் அகழாய்வில் தங்க நெட்டிப்பட்டம், வெண்கல பொருட்கள் கண்டெடுக்கப்பட்டுள்ளன.',
-#         '5) ஈலோன் மஸ்க்கின் ஸ்பேஸ் எக்ஸ் நிறுவனம் ஏராளமான செயற்கைக்கோள்களை வட்டப்பாதைக்கு அனுப்பிக்கொண்டிருக்கிறது.'
-#     ]
-
-
-#     cols = st.columns(len(texts)+2)
-#     cols[0].markdown('**Model**')
-#     cols[1].markdown('**Speaker**')
-#     for j, text in enumerate(texts):
-#         cols[j+2].markdown(f'**Text Prompt {j+1}**')
-
-#     for i, model in enumerate(models):
-#         cols = st.columns(len(texts)+2)
-#         cols[0].markdown(f"{model.split('|')[0]}: {model.split('|')[2]}")
-#         cols[1].markdown(model.split('|')[1])
-#         for j, text in enumerate(texts):
-#             with open(f"{model.split('|')[3]}/{j+1}.wav", 'rb') as file:
-#                 cols[j+2].audio(file.read())
-
-#     st.markdown("#### Text Prompts:")
-#     for j, text in enumerate(texts):
-#         st.markdown(text)
-#     ### End: Tamil
-
-# elif option == 'Hindi':
-#     ### START: Hindi
-#     st.markdown("### Hindi")
-
-#     models = [
-#         'AI4Bharat|Male|FastPitch+HiFiGAN|samples/samples_indictts_hi_male_fastpitch', 
-#         'Vakyansh|Male|GlowTTS+HiFiGAN|samples/samples_vakyansh_hi_male_glowtts', 
-#         'DonLab|Male|Tacotron2+WaveGlow|samples/samples_donlab_hi_male_tacotron2'
-#     ]
-
-#     texts = [
-#         "1) बिहार, राजस्थान और उत्तर प्रदेश से लेकर हरियाणा, मध्य प्रदेश एवं उत्तराखंड में सेना में भर्ती से जुड़ी 'अग्निपथ स्कीम' का विरोध जारी है.",
-#         "2) संयुक्त अरब अमीरात यानी यूएई ने बुधवार को एक फ़ैसला लिया कि अगले चार महीनों तक वो भारत से ख़रीदा हुआ गेहूँ को किसी और को नहीं बेचेगा.",
-#         "3) वाराणसी: ज्योतिष के छात्र अब बनेंगे आर्किटेक्ट, करेंगे इंटीरियर कोर्स.",
-#         "4) महाकुम्भ को भव्य और दिव्य बनाने की तैयारियां शुरू, पर्यावरण संरक्षण पर होगा फोकस.",
-#         "5) ब्याज दरों में वृद्धि के बावजूद नहीं घट रही मारुति की कारों की मांग, कई लाख ऑर्डर पैंडिंग."
-#     ]
-
-
-#     cols = st.columns(len(texts)+2)
-#     cols[0].markdown('**Model**')
-#     cols[1].markdown('**Speaker**')
-#     for j, text in enumerate(texts):
-#         cols[j+2].markdown(f'**Text Prompt {j+1}**')
-
-#     for i, model in enumerate(models):
-#         cols = st.columns(len(texts)+2)
-#         cols[0].markdown(f"{model.split('|')[0]}: {model.split('|')[2]}")
-#         cols[1].markdown(model.split('|')[1])
-#         for j, text in enumerate(texts):
-#             with open(f"{model.split('|')[3]}/{j+1}.wav", 'rb') as file:
-#                 cols[j+2].audio(file.read())
-
-#     st.markdown("#### Text Prompts:")
-#     for j, text in enumerate(texts):
-#         st.markdown(text)
-#     ### End: Hindi
-
-# elif option == 'Marathi':
-#     ### START: Marathi
-#     st.markdown("### Marathi")
-
-#     models = [
-#         'AI4Bharat|Male|FastPitch+HiFiGAN|samples/samples_indictts_mr_male_fastpitch', 
-#         #'Vakyansh|Male|GlowTTS+HiFiGAN|samples/samples_vakyansh_mr_male_glowtts', 
-#         'DonLab|Male|Tacotron2+WaveGlow|samples/samples_donlab_mr_male_tacotron2'
-#     ]
-
-#     texts = [
-#         "1) मविआ सरकार अल्पमतात आल्यानंतर अनेक निर्णय घेतले: मुख्यमंत्री एकनाथ शिंदे यांचा आरोप.",
-#         "2) वर्ध्यात भदाडी नदीच्या पुलावर कार डिव्हायडरला धडकून भीषण अपघात, दोघे गंभीर जखमी.", 
-#         "3) मुंबई पोलिसांना पाकिस्तानातून धमकीचा मेसेज, विरारमधून एक संशयित ताब्यात, पोलिसांकडून चौकशी सुरु.",
-#         "4) पुण्याची अनुष्का पारेख बनली कमी वेळात वजनदार डेडलिफ्ट उचलणारी भारतातील सर्वात 'तरुण' मुलगी.",
-#         "5) 'हॉटसीट'वर बसलेल्या तरुणीने सांगितले मुंबई लोकलमधील किस्से; ऐकून बिग बी देखील झाले खुश."
-#     ]
+if option in ['Kannada', 'Malayalam', 'Telugu']:
+    lang = {'Kannada': 'kn', 'Malayalam': 'ml', 'Telugu': 'te'}[option]
+    st.markdown(f'### option')
+    cols = st.columns(8)
+    cols[0].markdown('##### Index')
+    cols[1].markdown('##### Speaker')
+    cols[2].markdown('##### Emotion')
+    cols[3].markdown('##### v2: Expressive Voice (Emotive)')
+    cols[4].markdown('##### v2: Expressive Voice (Neutral)')
+    cols[5].markdown('##### v2: IndicTTS Voice (Emotive)')
+    cols[6].markdown('##### v2: IndicTTS Voice (Neutral)')
+    cols[7].markdown('##### v1: IndicTTS Voice')
+    st.markdown("""---""")
+    lang_files = sorted([f for f in files if lang in f])
+    indices = sorted(list(set([get_index(f) for f in lang_files])))
+    for index in indices:
+        index_files = [f for f in lang_files if index == get_index(f)]
+        print(index_files)
+        for speaker in ['female', 'male']:
+            speaker_files = filter_speakers(index_files, speaker)
+            emotion = speaker_files[0].split('_')[-1].replace('.wav', '')
+            if emotion == 'neutral':
+                emotion = speaker_files[1].split('_')[-1].replace('.wav', '')
+            
+            cols = st.columns(8)
+            cols[0].markdown(f'{index}')
+            cols[1].markdown(f'**{speaker.capitalize()}**')
+            cols[2].markdown(emotion)
+            print(speaker_files)
+        
+            speaker_file = f'{str(index)}_{lang}_{speaker}_{emotion}.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[5].audio(f.read())
+            
+            speaker_file = f'{str(index)}_{lang}_{speaker}_neutral.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[6].audio(f.read())
+            
+            speaker_file = f'{str(index)}_{lang}_{speaker}_v1.wav'
+            with open(os.path.join(samples_dir, speaker_file), 'rb') as f:
+                cols[7].audio(f.read())
+            
+        
 
 
-#     cols = st.columns(len(texts)+2)
-#     cols[0].markdown('**Model**')
-#     cols[1].markdown('**Speaker**')
-#     for j, text in enumerate(texts):
-#         cols[j+2].markdown(f'**Text Prompt {j+1}**')
 
-#     for i, model in enumerate(models):
-#         cols = st.columns(len(texts)+2)
-#         cols[0].markdown(f"{model.split('|')[0]}: {model.split('|')[2]}")
-#         cols[1].markdown(model.split('|')[1])
-#         for j, text in enumerate(texts):
-#             with open(f"{model.split('|')[3]}/{j+1}.wav", 'rb') as file:
-#                 cols[j+2].audio(file.read())
 
-#     st.markdown("Note: Vakyansh - Marathi model is not accessible")
-#     st.markdown("#### Text Prompts:")
-#     for j, text in enumerate(texts):
-#         st.markdown(text)
-#     ### End: Marathi
+
+
